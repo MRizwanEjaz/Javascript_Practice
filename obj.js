712b9fd2f7700a258bc6a081900bc3119f508f62
Muhammad Rizwan <rizviwp@protonmail.com>
2024-05-16 06:19:10 -0700

    Update obj.js

--- obj.js
+++ obj.js
@@ -10,3 +10,22 @@ const obj = {
 let a = console.log(obj.greet);
 console.log(obj.lastname);
 console.log(a);
+
+//obj cloning
+let src = {
+  name: "Rizwan",
+  age: "23",
+  height: "6 feet",
+};
+
+let src2 = {
+  value: "RizwanNofill",
+};
+
+// let dest = { ...src,};
+let dest = Object.assign({}, src, src2);
+
+// src.color = "white";
+
+console.log(src);
+console.log(dest);
